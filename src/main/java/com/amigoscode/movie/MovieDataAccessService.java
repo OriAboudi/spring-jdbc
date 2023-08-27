2dfd9276e742f8e8e93c33d5d6cb1a1ddf3bd1ce
OrAboudi <oriaboudi2001@gmail.com>
2023-08-27 17:57:39 +0300

    implements the methods insertMovie and selectMovie by jbdcTemplate and using in RowMapper for clean code

--- src/main/java/com/amigoscode/movie/MovieDataAccessService.java
+++ src/main/java/com/amigoscode/movie/MovieDataAccessService.java
@@ -3,20 +3,39 @@ package com.amigoscode.movie;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public class MovieDataAccessService implements MovieDao {
+    private final JdbcTemplate jdbcTemplate;
+
+    public MovieDataAccessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     @Override
     public List<Movie> selectMovies() {
-        throw new UnsupportedOperationException("not implemented");
+        var sql = """
+                SELECT id, name,release_date
+                FROM movie1
+                LIMIT 100;
+                """;
+        return jdbcTemplate.query(sql,new MovieRowMapper());
     }
 
     @Override
     public int insertMovie(Movie movie) {
-        throw new UnsupportedOperationException("not implemented");
+        String sql = """
+        INSERT INTO movie1(name,release_date )
+        VALUES (?,?);
+        """;
+        return jdbcTemplate.update(
+                sql,
+                movie.name(), movie.releaseDate()
+        );
+
     }
 
     @Override
